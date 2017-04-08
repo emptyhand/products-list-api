bcf077312217771d576c54379d6a60619ccaa557
shar <shar@shar.lt>
2017-04-08 19:03:10 +0300

    workaraund removed

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -3,14 +3,7 @@
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use AppBundle\DependencyInjection\Compiler\CustomPass;
 
 class AppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new CustomPass());
-    }
 }
